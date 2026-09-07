9bce9ba0b44d6e0e8c65204a74918746ff3330d2
bd102gm-hue <bd102gm@gmail.com>
2026-09-07 11:50:57 +0700

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development",
+  reloadOnOnline: true,
+});
+
+export default withSerwist({
+  reactStrictMode: true,
+});
